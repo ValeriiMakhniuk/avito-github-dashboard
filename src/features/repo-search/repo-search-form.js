622d55a5d286eaed72de30a100d4a259543a4580
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-06-14 15:17:31 +0300

    add RepoSearch Component

---
+++ src/features/repo-search/repo-search-form.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { useDebounce } from '../../hooks/useDebounce';
+
+import styles from './repo-search-form.module.css';
+
+export default function RepoSearhForm({
+  value,
+  setSearchedRepo,
+  saveRepoToLocalStorage,
+}) {
+  const [searchTerm, setSearchTerm] = useState(value);
+  const debounced = useDebounce(searchTerm, 1000);
+
+  const handleSearch = ({ target: { value } }) => {
+    setSearchTerm(value);
+  };
+
+  useEffect(() => {
+    if (debounced) {
+      setSearchedRepo(debounced);
+    }
+    saveRepoToLocalStorage(debounced);
+  }, [debounced, setSearchedRepo, saveRepoToLocalStorage]);
+
+  return (
+    <form action='/' className={styles.searchForm}>
+      <label htmlFor='search' className={styles.label}>
+        Search repositories
+      </label>
+      <input
+        type='text'
+        value={searchTerm}
+        onChange={handleSearch}
+        id='search'
+        className={styles.input}
+        placeholder='Search'
+      />
+    </form>
+  );
+}
