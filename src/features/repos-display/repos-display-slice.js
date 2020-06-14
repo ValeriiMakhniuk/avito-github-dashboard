d04b0faaf28ee1b207db86bfe8fd901dc0f6f0de
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-06-14 15:16:35 +0300

    add repos-display-slice

---
+++ src/features/repos-display/repos-display-slice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { displayTypes } from '../../constants';
+
+const initialState = {
+  repo: '',
+  page: 1,
+  displayType: displayTypes.REPO_LIST,
+  activeRepoId: null,
+};
+
+const reposDisplaySlice = createSlice({
+  name: 'reposDisplay',
+  initialState,
+  reducers: {
+    setCurrentDisplayType(state, { payload }) {
+      const { displayType, repoId = null } = payload;
+      state.displayType = displayType;
+      state.activeRepoId = repoId;
+    },
+    setCurrentPage(state, { payload }) {
+      state.page = payload;
+    },
+    setRepo(state, { payload }) {
+      state.repo = payload;
+    },
+  },
+});
+
+export const {
+  setCurrentDisplayType,
+  setCurrentPage,
+  setRepo,
+} = reposDisplaySlice.actions;
+
+export default reposDisplaySlice.reducer;
