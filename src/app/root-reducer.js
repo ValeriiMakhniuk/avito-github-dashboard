24e090ee660d37d5e90c233ebb439ffd704a96c1
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-06-15 13:31:49 +0300

    add repos reducer

--- src/app/root-reducer.js
+++ src/app/root-reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import reposDisplayReducer from '../features/repos-display/repos-display-slice';
+import reposReducer from '../features/repo-list/repo-list-slice';
 
 const rootReducer = combineReducers({
   reposDisplay: reposDisplayReducer,
+  repos: reposReducer,
 });
 
 export default rootReducer;
