69445819149f247307e3db65f155cc3eb599ed60
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-06-15 13:27:36 +0300

    add RepoListPage component

---
+++ src/features/repo-list/RepoListPage.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { fetchRepos, fetchTopRepos } from './repo-list-slice';
+
+import { RepoList } from './RepoList';
+
+import styles from './RepoListPage.module.css';
+
+export function RepoListPage({ jumpToPage, showRepoDetails, repo, page }) {
+  const dispatch = useDispatch();
+  const {
+    currentPageRepos,
+    isLoading,
+    error: reposError,
+    byId,
+    pageCount,
+  } = useSelector((state) => state.repos);
+
+  const repos = currentPageRepos.map((id) => byId[id]);
+
+  useEffect(() => {
+    if (repo === '') {
+      dispatch(fetchTopRepos());
+    } else {
+      dispatch(fetchRepos(repo, page));
+    }
+  }, [repo, page, dispatch]);
+
+  if (reposError) {
+    return (
+      <div>
+        <h1>Something went wrong...</h1>
+        <div>{reposError}</div>
+      </div>
+    );
+  }
+
+  const currentPage = Math.min(pageCount, Math.max(page, 1)) - 1;
+
+  let renderedList = isLoading ? (
+    <h3>Loading...</h3>
+  ) : (
+    <RepoList repos={repos} showRepoDetails={showRepoDetails} />
+  );
+
+  const onPageChanged = (selectedItem) => {
+    const newPage = selectedItem.selected + 1;
+    jumpToPage(newPage);
+  };
+
+  return (
+    <main>
+      <div className={styles.pageInner}>{renderedList}</div>
+      {/* <RepoPagination
+        currentPage={currentPage}
+        pageCount={pageCount}
+        onPageChange={onPageChanged}
+      /> */}
+    </main>
+  );
+}
